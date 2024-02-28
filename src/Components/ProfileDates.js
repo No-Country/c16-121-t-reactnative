2461035a3630a630e7094331728857f0700b160b
Lucía Costamagna <luci3220@hotmail.com>
2024-02-28 20:06:20 -0300

    se agrego funciones para modificar la db y cambios en componente profile

--- src/Components/ProfileDates.js
+++ src/Components/ProfileDates.js
@@ -1,95 +1,112 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import * as React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import { Colors } from "../Constants/Colors";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 
 const ProfileDates = () => {
+  // const username = attributes && attributes.name ? attributes.name : '';
+
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.card}>
-          <Text style={styles.text}>Nombre</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Información adicional"
-            placeholderTextColor="white"
-          />
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.text}>Edad</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Información adicional"
-            placeholderTextColor="white"
-          />
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.card}>
-          <Text style={styles.text}>Fecha de nacimiento</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Información adicional"
-            placeholderTextColor="white"
-          />
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.text}>Pais-Ciudad de procedencia  <FontAwesome name="map-marker" size={16} color="white" /></Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Información adicional"
-            placeholderTextColor="white"
-          />
-        </View>
-      </View>
+      <Text style={styles.sectionTitle}> INFORMACION DEL USUARIO: </Text>
+
+      <InfoDate label={"Nombre"} canEdit value={"LUCIA"} />
+      <InfoDate label={"Edad   "} canEdit value={"LUCIA"} />
+      <InfoDate label={"Nacimiento"} canEdit value={"LUCIA"} />
+      <InfoDate label={"Residencia"} canEdit value={"LUCIA"} />
     </View>
   );
 };
+function InfoDate({ label, value, canEdit, handleUpDate, handleContext }) {
+  const [localValue, setLocalValue] = React.useState(value);
+  return (
+    <View style={styles.fielContainer}>
+      <Text
+        style={styles.label}
+      >
+        {" "}
+        {label}
+      </Text>
+      <TextInput
+        placeholder={label}
+        keyboardType={canEdit ? "web-search" : "default"}
+        onChangeText={canEdit && setLocalValue}
+        value={localValue}
+        onSubmitEditing={() => alert("hello")}
+        style={{
+          fontWeight: "500",
+          flexShrink: 1,
+          marginHorizontal:2,
+         
+        }}
+      />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+   
+  },
+  sectionTitle: {
+    fontSize: 16,
+ color:'grey',
+    marginBottom: 1,
+  },
+  fielContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingVertical: 11,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 10,
+  },
+  label:{
+    flex: 1,
+    fontWeight: "500",
+    color: "grey",
+    marginRight: 18,
+    fontSize: 16,
+    
+ 
   },
   card: {
     flex: 1,
     height: 100,
-    backgroundColor:Colors.profileCard,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: Colors.profileCard,
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
-    marginHorizontal: 3, 
-    borderRadius:15,
-    shadowColor: '#000',
+    marginHorizontal: 3,
+    borderRadius: 15,
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 3,
     },
     shadowOpacity: 0.3,
     shadowRadius: 5,
-    elevation: 6, 
+    elevation: 6,
   },
   input: {
-    width: '100%',
+    width: "100%",
     marginBottom: 10,
     padding: 5,
     borderWidth: 1,
-    borderColor: 'transparent',
-    color:'white',
+    borderColor: "transparent",
+    color: "white",
     borderRadius: 5,
   },
-  text:{
-    fontWeight:'bold',
-    color:'white',
-    fontSize:16
-  }
+  text: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 16,
+  },
 });
 
 export default ProfileDates;
