bcf871f7be2ab8c4441f9cef5306b2ef15873b65
Lucía Costamagna <luci3220@hotmail.com>
2024-02-28 23:06:40 -0300

    se tomo el nombre del usaurio para completar campo, se corrigieron errores

--- src/Navigation/Tabs.js
+++ src/Navigation/Tabs.js
@@ -11,71 +11,74 @@ import Exit from "../Screens/Exit";
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: "#FCC5D2",
+          borderTopColor: "white",
+          borderTopWidth: 2,
+          paddingTop: 8,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        },
+        tabBarIconStyle: {
+          marginBottom: -1,
+        },
+        tabBarLabelStyle: {
+          color: "black",
+          fontSize: 12,
+          marginBottom: 3,
+        },
 
-     
-
-    return (
-        <Tab.Navigator screenOptions={{
-            headerShown: false,
-            tabBarStyle: {
-              backgroundColor: '#FCC5D2',
-              borderTopColor: 'white', 
-              borderTopWidth: 2, 
-              paddingTop: 8, 
-              borderTopLeftRadius: 30, 
-              borderTopRightRadius: 30,
-            },
-            tabBarIconStyle: {
-              marginBottom: -1, 
-            },
-            tabBarLabelStyle: {
-              color:'black',
-              fontSize: 12, 
-              marginBottom: 3, 
-            },
-            tabBarInactiveTintColor: '#666666', 
-            tabBarActiveTintColor: '#F3305F', 
-            tabBarPressColor: 'rgba(243, 48, 95, 0.7)'
-          }}>
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="home" color={color} size={size} />
-                ),
-                }}
-                
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="person" color={color} size={size} />
-                ),
-                }}
-            />
-            <Tab.Screen
-                name="DonationForm"
-                component={DonationForm}
-                options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="circle" color={color} size={size} />
-                ),
-                }}
-            />
-            <Tab.Screen
-                name="Exit"
-                component={Exit}
-                options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="exit-to-app" color={color} size={size} />
-                ),
-                }}
-            />
-        </Tab.Navigator>
-    );
+        tabBarInactiveTintColor: "#666666",
+        tabBarActiveTintColor: "#F3305F",
+        tabBarPressColor: "rgba(243, 48, 95, 0.7)",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="DonationForm"
+        component={DonationForm}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="circle" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Exit"
+        component={Exit}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="exit-to-app" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 // export default function Navigation() {
