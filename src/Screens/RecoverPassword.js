8706cf45011eec2212ea940e5960cd5b4e87eb82
Lucía Costamagna <luci3220@hotmail.com>
2024-03-02 19:44:15 -0300

    estilos

--- src/Screens/RecoverPassword.js
+++ src/Screens/RecoverPassword.js
@@ -4,28 +4,28 @@ import ForgotPassword from "../Components/ForgotPassword";
 import Background from "../Components/Background";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // marginTop: 300,
-        alignItems: "center", // Centra el contenido horizontalmente
-    },
-    scrollViewContainer: {
-        // flex: 1,
-        marginTop: 290,
-        alignItems: "center",
-    },
+  container: {
+   
+    marginTop: 290,
+    alignItems: "center", // Centra el contenido horizontalmente
+  },
+  scrollViewContainer: {
+    // flex: 1,
+    marginTop: 290,
+    alignItems: "center",
+  },
 });
 
 export default function RecoverPassword() {
-    return (
-        /**Para evitar que el teclado del dispositivo abra y mueva el formulario hacia arriba
-         * uso de la propiedad keyboardShouldPersistTaps en el componente ScrollView.
-         * Esto evita que el teclado cause un desplazamiento de la vista cuando se toque un campo */
-        <View style={styles.container}>
-            <Background />
-            <ScrollView contentContainerStyle={styles.scrollViewContainer} keyboardShouldPersistTaps="handled" >
-                <ForgotPassword />
-            </ScrollView>
-        </View>
-    );
+  return (
+    /**Para evitar que el teclado del dispositivo abra y mueva el formulario hacia arriba
+     * uso de la propiedad keyboardShouldPersistTaps en el componente ScrollView.
+     * Esto evita que el teclado cause un desplazamiento de la vista cuando se toque un campo */
+    <View>
+      <Background />
+      <View style={styles.container}>
+        <ForgotPassword />
+      </View>
+    </View>
+  );
 }
