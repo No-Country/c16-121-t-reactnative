37328df2a7f7be542ad1fedff7ba99116a04d43f
Mariana Potichkin <marpotichkin@gmail.com>
2024-03-03 16:18:54 -0300

    cambios en los estilos en perfil y datos de donación

--- src/Components/DateInput.js
+++ src/Components/DateInput.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import { View, TextInput, Modal, Platform, StyleSheet, TouchableOpacity } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Modal,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { Colors } from "../Constants/Colors";
 
 const DateInput = ({ label, value, onChange }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedDate, setSelectedDate] = useState(value);
-  const [dateString, setDateString] = useState(value ? value.toLocaleDateString() : '')
+  const [dateString, setDateString] = useState(
+    value ? value.toLocaleDateString() : ""
+  );
 
   const handleModalOpen = () => {
     setModalVisible(true);
@@ -19,20 +28,23 @@ const DateInput = ({ label, value, onChange }) => {
   const handleChange = (event, selectedDate) => {
     if (selectedDate) {
       setSelectedDate(selectedDate);
-      setDateString(selectedDate.toLocaleDateString())
+      setDateString(selectedDate.toLocaleDateString());
       onChange(selectedDate);
       handleModalClose();
     }
   };
+
+  const textColor = selectedDate && "black";
 
   return (
     <View>
       <TouchableOpacity onPress={handleModalOpen}>
         <TextInput
           label={label}
-          value={dateString || '  05/02/1994'}
-          style={styles.input}
+          value={dateString || "05/02/1994"}
+          style={[styles.input, { color: textColor }]}
           editable={false}
+          textAlign="center"
         />
       </TouchableOpacity>
       <Modal
@@ -41,9 +53,12 @@ const DateInput = ({ label, value, onChange }) => {
         transparent={true}
         onRequestClose={handleModalClose}
       >
-        <TouchableOpacity style={styles.modalContainer} onPress={handleModalClose}>
+        <TouchableOpacity
+          style={styles.modalContainer}
+          onPress={handleModalClose}
+        >
           <View style={styles.modalContent}>
-            <DateTimePicker 
+            <DateTimePicker
               value={selectedDate || new Date()}
               mode="date"
               display="spinner"
@@ -58,26 +73,24 @@ const DateInput = ({ label, value, onChange }) => {
 };
 
 const styles = StyleSheet.create({
-    input: {
-        width: "90%",
-        height: 45,
-        justifyContent: "center",
-        margin: 10,
-        backgroundColor: 'transparent',
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: Colors.input,
-      },
+  input: {
+    width: "90%",
+    height: 45,
+    justifyContent: "center",
+    margin: 10,
+    backgroundColor: "transparent",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.input,
+  },
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
-    backgroundColor: 'transparent',
-    // borderRadius: 10,
-    // padding: 20,
+    backgroundColor: "transparent",
   },
 });
 
