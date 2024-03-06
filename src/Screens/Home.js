73726781cee4b72437396b3e7850522b0b0bfcc3
verocampero <verocampero2@gmail.com>
2024-03-05 23:23:13 -0300

    modoDarck

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -5,6 +5,7 @@ import { PostCard } from "../Components/postCard/PostCard";
 import CardHome from "../Components/CardHome";
 import { DarckContext } from "../Context/DarckContext";
 import { useContext } from "react";
+
 const data = [
   {
     name: "laura lopez",
@@ -18,60 +19,14 @@ const data = [
     image:
       "https://purina.com.pe/sites/default/files/styles/webp/public/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg.webp?itok=N2sS0lfp",
   },
-  // {
-  //   name: "maria lopez",
-  //   city: "Buenos Aires",
-  //   country: "",
-  //   location: "Hospital General del Niño",
-  //   contact: "4645564156",
-  //   coment:
-  //     "With React Native, you style your application using JavaScript. All of the core components accept a prop named style. The style names and values usually match how CSS works on the ith React Native, you style your application usin .",
-  //   type: "+A",
-  //   image:'https://st4.depositphotos.com/1325564/19966/i/450/depositphotos_199669506-stock-photo-beautiful-kitten-scottish-fold-play.jpg'
-  // },
-  // {
-  //   name: "sofia lopez",
-  //   city: "Buenos Aires",
-  //   country: "",
-  //   location: "Hospital General del Niño",
-  //   contact: "4645564156",
-  //   coment:
-  //     "With React Native, you style your application using JavaScript. All of the core componentsyou style your application using JavaScript. All of the core components  accept a prop named style. The style names and values usually match how CSS works on the ith React Native, you style your application usin .",
-  //   type: "+A",
-  //   image:'https://planetamascotaperu.com/wp-content/uploads/2021/04/Llamame-Gatito.jpg'
-  // },
-  // {
-  //   name: "pedro lopez",
-  //   city: "Buenos Aires",
-  //   country: "",
-  //   location: "Hospital Hipólito Unanue",
-  //   contact: "4645564156",
-  //   coment:
-  //     "With React Native, you style your application using JavaScript. All of the core components accept a prop named style. The style names andyou style your application using JavaScript. All of the core components  values usually match how CSS works on the ith React Native, you style your application usin . The style names andyou style your application using JavaScript. All of the core components  values usually match how CSS works on the ith React Native, you style your application usi .",
-  //   type: "+A",
-  //   image:"https://purina.com.pe/sites/default/files/styles/webp/public/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg.webp?itok=N2sS0lfp"
-
-  // },
-  // {
-  //   name: "clara lopez",
-  //   city: "Buenos Aires",
-  //   country: "",
-  //   location: "Hospital General del Niño",
-  //   contact: "4645564156",
-  //   coment:
-  //     "With React Native, you style your application using JavaScript. All of the core components accept a prop named style. The style names and values usually match how CSS works on the ith React Native, you style your application usin .",
-  //   type: "+A",
-  //   image:"https://purina.com.pe/sites/default/files/styles/webp/public/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg.webp?itok=N2sS0lfp"
-
-  // },
 ];
 
 const Home = () => {
-
   const { theme } = useContext(DarckContext);
-  const { background, margin,  height, borderBottomLeftRadius, borderBottomRightRadius } = theme;
+  const { background } = theme;
+
   return (
-    <SafeAreaView style={[{ flex: 1, backgroundColor: "white" }]}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: background }}>
       <View style={{ flex: 3 / 5 }}>
         <HeaderMovil condition={true}></HeaderMovil>
       </View>
@@ -85,17 +40,6 @@ const Home = () => {
 
       <CardHome />
     </SafeAreaView>
-    /* <View style={{ flex: 5/7
-    }}>
-
-          <FlatList
-          horizontal={true}
-          data={data}
-          renderItem={({ item }) => <PostCard itemProfile={item}></PostCard>}
-          keyExtractor={(item) => item.name}
-        ></FlatList>
-     
-      </View>  */
   );
 };
 
--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { View, ScrollView, StyleSheet } from "react-native";
-
+import { useContext } from "react";
+import { DarckContext } from "../Context/DarckContext";
 import { Hub } from "aws-amplify";
 import  SignIn  from "../Components/SignIn";
 import Background from "../Components/Background";
 import { AuthProvider } from "../Context/AuthContext";
 
 const styles = StyleSheet.create({
+    
     container: {
         flex: 1,
+      
+       
     },
     scrollViewContainer: {
         // flex: 1,
@@ -18,6 +22,10 @@ const styles = StyleSheet.create({
 });
 
 export default function Login({ navigation }) {
+
+    const { theme } = useContext(DarckContext);
+    const { background} = theme;
+  
     const [user, setUser] = React.useState(null);
 
     const listener = (data) => {
@@ -42,7 +50,7 @@ export default function Login({ navigation }) {
          * Esto evita que el teclado cause un desplazamiento de la vista cuando se toque un campo */
         
         <AuthProvider navigation={navigation}>
-        <View style={styles.container}>
+        <View style={[styles.container, {backgroundColor:background}]}>
             <Background/>
             <ScrollView contentContainerStyle={styles.scrollViewContainer} keyboardShouldPersistTaps="handled" >
             
